63c007e82c431cea58ce3db25512606bc0a4ca93
Christy Breslin <christybreslin@Mac.localdomain>
2025-03-26 08:34:36 +0000

    Configure development and production ports (3000/3005)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env: {
+    PORT_DEV: process.env.PORT_DEV,
+    PORT_PROD: process.env.PORT_PROD,
+  },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
